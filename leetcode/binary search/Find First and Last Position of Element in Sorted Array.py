75956ba2147db3668fa98157de301c72f7453e63
VictorDing22 <dse250073@gmail.com>
2020-04-19 01:23:15 +1000

    binary search init

---
+++ leetcode/binary search/Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,13 @@
+# 34. Find First and Last Position of Element in Sorted Array
+
+
+class Solution:
+
+    def searchRange(self, nums, target):
+        return
+
+
+
+nums = [5, 7, 7, 8, 8, 10]
+s = Solution()
+print(s.searchRange(nums, 8))
