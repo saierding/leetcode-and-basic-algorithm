a0cc0bc6bcbcec033a7b087fbdbf2b9dab35e93f
VictorDing22 <dse250073@gmail.com>
2020-05-27 01:10:22 +1000

    dp

---
+++ leetcode/dynamic programming/Climbing Stairs.py
@@ -0,0 +1,21 @@
+# 70. Climbing Stairs
+
+
+class Solution:
+
+    def climbStairs(self, n):
+        fib_front = 1
+        fib_rear = 1
+        if n < 0:
+            return 0
+        if n == 1:
+            return 1
+        for i in range(2, n+1):
+            ans = fib_front + fib_rear
+            fib_front = fib_rear
+            fib_rear = ans
+        return ans
+
+
+s = Solution()
+print(s.climbStairs(3))
