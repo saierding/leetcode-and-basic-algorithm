0f1debe742caa24ba64cf0e65bc85eca5736e718
VictorDing22 <dse250073@gmail.com>
2020-08-20 17:12:27 +1000

    a star

--- test.py
+++ test.py
@@ -1,29 +1,25 @@
-def aStarSearch(problem, heuristic=nullHeuristic):
-    import copy
-    open = util.PriorityQueue()
-    starth = heuristic(problem.getStartState(), problem)
-    startg = 0
-    startTrack = [None]
-    initCost = 0
-    bestG = 0
-    start_node = [problem.getStartState(), startTrack, initCost]
-    open.push(start_node, startg + 2*starth)
-    closed = []
-    best_g = {problem.getStartState(): 100000}
-    while not open.isEmpty():
-        s = open.pop()
-        if s[0] not in closed or s[2] < best_g.get(s[0]):
-            closed.append(s[0])
-            best_g[s[0]] = s[2]
-            if problem.isGoalState(s[0]):
-                return s[1][1:]
-            for c in problem.getSuccessors(s[0]):
-                ch = heuristic(c[0], problem)
-                cg = s[2] + c[2]
-                Closed = copy.deepcopy(s[1])
-                Closed.append(c[1])
-                cData = [c[0], Closed, cg]
-                if c[0] not in closed:
-                    best_g[c[0]] = cg
-                if heuristic(c[0], problem)<10000:
-                    open.push(cData, cg + 2*ch)
+def aStarSearch(self,gameState,goal,heuristic):
+    """
+    astar的方法，heuristic用上面的发现敌人赶紧跑的策略
+    """
+    start =self.getCurrentObservation().getAgentState(self.index).getPosition()
+    openSet = util.PriorityQueue()
+    openSet.push(( start,[]), 0)
+    visitedNodes = []
+    while not openSet.isEmpty():
+      node,trace=openSet.pop()
+      if node == goal:
+        if len(trace)==0:
+          return 'Stop'
+
+        return trace[0]
+      if node not in visitedNodes:
+        visitedNodes.append(node)
+        successors=self.getSuccessors(node)
+        for successor in successors:
+          cost= len(trace +[successor[1]])+heuristic(gameState,successor[0])
+          if successor not in visitedNodes:
+            openSet.push((successor[0], trace + [successor[1]]), cost)
+    if goal != self.home:
+      return self.aStarSearch(gameState,self.home,self.enemyConcernHeuristic)
+    return 'Stop'
